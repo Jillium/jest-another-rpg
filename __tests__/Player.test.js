0b0493f32c83c71137e72098b35abb1187623c9a
Jillium <86424914+Jillium@users.noreply.github.com>
2021-11-08 12:49:29 -0700

    added constructors for getinventory and getstats

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,3 @@
-const { expect } = require('@jest/globals');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
@@ -17,3 +16,22 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test("get's player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+})
