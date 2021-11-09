de2ef0026ab01523632abd436b04cd7c1023ded0
Jillium <86424914+Jillium@users.noreply.github.com>
2021-11-09 09:34:41 -0700

    player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -34,4 +34,33 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
-})
+});
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+  });
+
+  test('checks if player is alive or not', () => {
+      const player = new Player('Dave');
+
+      expect(player.isAlive()).toBeTruthy();
+
+      player.health = 0;
+
+      expect(player.isAlive()).toBeFalsy();
+  });
+
+  test("subtracts from player's health", () => {
+      const player = new Player('Dave');
+      const oldHealth = player.health;
+
+      player.reduceHealth(5);
+
+      expect(player.health).toBe(oldHealth - 5);
+
+      player.reduceHealth(99999);
+
+      expect(player.health).toBe(0);
+  });
